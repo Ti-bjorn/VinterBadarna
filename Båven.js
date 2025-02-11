116163e5195e269d7f1edde165f673fbe0db699e
Ti-bjorn <torbjornfries@gmail.com>
2025-02-11 15:16:46 +0100

    made the dropdown menu work on live server

--- Båven.js
+++ Båven.js
@@ -1,11 +1,17 @@
-function toggleDropdown() {
-    var dropdown = document.getElementById("dropdownMenu");
-    if (dropdown.style.display === "inline") {
+function toggleDropdown3() {
+    const dropdown = document.getElementById("dropdownMenu");
+    if (dropdown.style.display === "block") {
         dropdown.style.display = "none";
     } else {
-        dropdown.style.display = "inline";
+        dropdown.style.display = "block";
     }
 }
 function toggleDropdown2() {
     document.getElementById("dropdownMenu").classList.toggle("show");
 }
+
+function toggleDropdown(e) {
+    e.preventDefault(); // Prevent the default link behavior
+    const dropdown = document.getElementById("dropdownMenu");
+    dropdown.classList.toggle("d-none"); // Toggle the Bootstrap 'd-none' class
+  }
