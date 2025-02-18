988aeb206f5e39cc934e9dc1160d33fb60baddae
Ti-bjorn <torbjornfries@gmail.com>
2025-02-18 12:38:22 +0100

    Added content for most pages

--- Båven.js
+++ Båven.js
@@ -1,17 +1,6 @@
-function toggleDropdown3() {
-    const dropdown = document.getElementById("dropdownMenu");
-    if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
-    } else {
-        dropdown.style.display = "block";
-    }
-}
-function toggleDropdown2() {
-    document.getElementById("dropdownMenu").classList.toggle("show");
-}
 
 function toggleDropdown(e) {
-    e.preventDefault(); // Prevent the default link behavior
+    e.preventDefault();
     const dropdown = document.getElementById("dropdownMenu");
-    dropdown.classList.toggle("d-none"); // Toggle the Bootstrap 'd-none' class
+    dropdown.classList.toggle("d-none");
   }
