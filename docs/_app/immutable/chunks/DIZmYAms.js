4b6d675afb0adfe35b26f116dc9cc9e081b317ee
Torbjörn Fries <torfri@nutanixdev.local>
2025-10-07 17:36:37 +0200

    rebuilt docs again again

--- docs/_app/immutable/chunks/DIZmYAms.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./CfUyi4ht.js";a();
--- docs/_app/immutable/nodes/6.ZRk4psSR.js
+++
@@ -1,6 +0,0 @@
-import"../chunks/DsnmJJEf.js";import"../chunks/DIZmYAms.js";import{x as a,y as t,z as n}from"../chunks/CfUyi4ht.js";var i=a(`<div class="textruta1"><p>Här återfinns mötesprotokoll för tidigare möten, noggrant dokumenterade enligt vedertagna principer för Båven
-      Lovers verksamhet. Varje protokoll innehåller en detaljerad redogörelse för beslut, diskussioner och
-      observationer.
-
-      Samtliga dokument är upprättade i enlighet med föreningens stadgar och finns tillgängliga för medlemmar som önskar
-      fördjupa sig i Båven Lovers organisatoriska dimensioner.</p></div> <div class="textruta1"><h3>Mötesprotokoll för åren:</h3> <ul><li><a class="textlink">2025</a></li> <li><a class="textlink" href="VinterbadarnasArsmöte2023December.pdf" target="_blank">2024</a></li> <li><a class="textlink" href="Vinterbadarnasårsmöte2023.pdf" target="_blank">2023</a></li> <li><a class="textlink" href="Vinterbadarna2021.pdf" target="_blank">2021</a></li></ul></div>`,1);function d(e){var r=i();n(2),t(e,r)}export{d as component};
